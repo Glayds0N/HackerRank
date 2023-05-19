80d974ffeb26ab0d0c92db77cc4a15a5999beeeb
Glaydson Hastenreiter <dev.glaydson.ch@gmail.com>
2023-05-19 08:29:18 -0300

    My first problem on HackerRank

---
+++ 01 - Solve me first/solve-me-first.js
@@ -0,0 +1,10 @@
+
+const a = Math.floor(Math.random() * 1000);
+const b = Math.floor(Math.random() * 1000);
+
+function solveMeFirst(a, b){
+    // My code!
+    return a + b
+}
+
+console.log(solveMeFirst(a, b));
