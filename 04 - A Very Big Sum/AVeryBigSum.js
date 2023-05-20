7c82cc801681ab4725f771588550a22d650cb886
Glaydson Hastenreiter <dev.glaydson.ch@gmail.com>
2023-05-20 14:30:19 -0300

    My fourth problem on HackerRank

---
+++ 04 - A Very Big Sum/AVeryBigSum.js
@@ -0,0 +1,25 @@
+// 1 <= n <=10
+const n = Math.floor(Math.random() * 10) + 1;
+
+// 0 <= ar[i] <=10^10
+const ar = [];
+for (let index = 0; index < n; index++) {
+    const i = Math.floor(Math.random()*(10**10) + 1);
+    ar.push(i);
+}
+
+
+// My Code!
+function aVeryBigSum(ar){
+    let total = 0;
+    for (const value of ar) {
+        total += value;
+    }
+    return total;
+}
+
+console.log(n);
+console.log(ar);
+console.log(aVeryBigSum(ar));
+
+
